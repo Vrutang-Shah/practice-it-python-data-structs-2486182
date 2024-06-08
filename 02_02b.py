81434e44a21d672b65ba843f40856002a6a2c481
Vrutang-Shah <102398104+Vrutang-Shah@users.noreply.github.com>
2024-06-08 17:59:43 +0000

    Deque Practice

--- 02_02b.py
+++ 02_02b.py
@@ -1,7 +1,17 @@
+from collections import deque
+# from pprint import pprint
+
+def check_palindrome(word):
+    word = deque(word)
+    while len(word) > 1:
+        if word.pop() != word.popleft():
+            return False
+    return True
 
 def main():
     #add code here
     return
 
 if __name__ == "__main__":
-    main()
+    # main()
+    print(check_palindrome("tacocat"))
--- 02_03b.py
+++ 02_03b.py
@@ -1,6 +1,40 @@
+from collections import deque
+
+class Task:
+    def __init__(self, task_name, hasPriority = 'False'):
+        self.task_name = task_name
+        self.hasPriority = hasPriority
+
+tasklist = deque()
+def add_task(task):
+    if task.hasPriority == True:
+        tasklist.appendleft(task.task_name)
+    else:
+        tasklist.append(task.task_name)
+
+def complete_task():
+    return tasklist.popleft()
+
+def print_task():
+    for each in tasklist:
+        print(each)
 
 def main():
     #add code here
+    print(tasklist)
+    add_task(Task("Name List"))
+    print(len(tasklist))
+    print(tasklist)
+    add_task(Task("Name List Priority", hasPriority=True))
+    print(len(tasklist))
+    print(tasklist)
+    add_task(Task("Name List Non Priority"))
+    print(len(tasklist))
+    print(tasklist)
+    complete_task()
+    print(len(tasklist))
+    print(tasklist)
+    print_task()
     return
 
 if __name__ == "__main__":
